911d42f4164cc6eb01b04662926fa5f7a6616ab9
Max Peng <pengisgood@gmail.com>
2014-11-29 23:35:34 +0800

    add sanity test

---
+++ c3-java-tdd-gradle/src/test/java/com/makedream/SanityTest.java
@@ -0,0 +1,21 @@
+package com.makedream;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SanityTest {
+    @Test
+    public void should_one_plus_two_equal_three() throws Exception {
+        //准备数据
+        int one = 1;
+        int two = 2;
+
+        //执行方法
+        int actual = one + two;
+
+        //验证结果
+        assertThat(actual, is(3));
+    }
+}
