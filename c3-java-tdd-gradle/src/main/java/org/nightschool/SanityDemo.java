de37aeda2c19ac1867bfae71cd7fc762de7ece52
Max Peng <pengisgood@gmail.com>
2014-11-30 17:54:11 +0800

    卖的一手好萌😄

---
+++ c3-java-tdd-gradle/src/main/java/org/nightschool/SanityDemo.java
@@ -0,0 +1,19 @@
+package org.nightschool;
+
+public class SanityDemo {
+    //example
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    //a half done work
+    public int minus(int num1, int num2) {
+        return 0;
+    }
+
+    //Todo: implement the multiply method
+//    public int multiply(int num1, int num2) {
+//
+//    }
+
+}
