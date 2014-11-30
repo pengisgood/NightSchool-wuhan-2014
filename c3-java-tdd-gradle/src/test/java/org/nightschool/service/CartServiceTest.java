4b2ca5b8b6cac1279cde4fef08a48cf84a2390de
Max Peng <pengisgood@gmail.com>
2014-12-01 00:45:34 +0800

    第一次重构，去掉重复代码

--- c3-java-tdd-gradle/src/test/java/org/nightschool/service/CartServiceTest.java
+++ c3-java-tdd-gradle/src/test/java/org/nightschool/service/CartServiceTest.java
@@ -1,5 +1,6 @@
 package org.nightschool.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.nightschool.model.Disk;
 
@@ -10,10 +11,16 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class CartServiceTest {
+
+    private CartService cartService;
+
+    @Before
+    public void setUp() throws Exception {
+        cartService = new CartService();
+    }
+
     @Test
     public void should_cart_be_empty_when_nothing_added() throws Exception {
-        CartService cartService = new CartService();
-
         List<Disk> disks = cartService.getDisks();
 
         assertThat(disks.size(), is(0));
@@ -21,7 +28,6 @@ public class CartServiceTest {
 
     @Test
     public void should_able_to_add_disk_to_cart() throws Exception {
-        CartService cartService = new CartService();
         Disk disk = new Disk("小清新光盘", "../images/disk/fancy-disk.jpg", "小清新、小文艺 35元/10张");
 
         cartService.addDisk(disk);
