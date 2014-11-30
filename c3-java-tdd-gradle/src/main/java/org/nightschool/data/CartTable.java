7cd5120254513c7b99774906c2149cba397003a1
Max Peng <pengisgood@gmail.com>
2014-11-30 20:13:19 +0800

    修好第一个测试

--- c3-java-tdd-gradle/src/main/java/org/nightschool/data/CartTable.java
+++ c3-java-tdd-gradle/src/main/java/org/nightschool/data/CartTable.java
@@ -2,9 +2,10 @@ package org.nightschool.data;
 
 import org.nightschool.model.Disk;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartTable {
-    public List<Disk> disks;
+    public List<Disk> disks = new ArrayList<>();
 
 }
