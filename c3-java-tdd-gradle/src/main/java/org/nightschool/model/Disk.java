19699225f3766e4a9f7e74af5d95e94483de4e4c
Max Peng <pengisgood@gmail.com>
2014-12-02 12:53:27 +0800

    pre-class workspace

--- c3-java-tdd-gradle/src/main/java/org/nightschool/model/Disk.java
+++
@@ -1,7 +0,0 @@
-package org.nightschool.model;
-
-public class Disk {
-    public Disk(String name, String imgUrl, String desc) {
-
-    }
-}
--- c3-java-tdd-gradle/src/test/java/org/nightschool/demo/CalculatorTest.java
+++ c3-java-tdd-gradle/src/test/java/org/nightschool/demo/CalculatorTest.java
@@ -2,7 +2,6 @@ package org.nightschool.demo;
 
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
-import org.nightschool.demo.Calculator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
