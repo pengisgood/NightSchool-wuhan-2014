296bf0fcd2affe2042f741beeaa7ec024bb18679
Max Peng <pengisgood@gmail.com>
2015-04-08 15:24:03 +0800

    change driver to chrome

--- c7-e2e-test/src/test/java/com/thoughtworks/FunctionalTest.java
+++ c7-e2e-test/src/test/java/com/thoughtworks/FunctionalTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -25,7 +25,7 @@ public class FunctionalTest {
             System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         }
 
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
     }
 
     @After
